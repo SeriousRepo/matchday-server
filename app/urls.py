dc0ca81b790fb44577d6212bd19550aec456e237
SeriousRepo <etekef.ro.man@gmail.com>
2018-07-28 00:27:27 +0200

    urls change

--- app/urls.py
+++ app/urls.py
@@ -3,12 +3,12 @@ from app import views
 
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'persons', views.PersonViewSet)
-router.register(r'players', views.PlayerViewSet)
-router.register(r'matches', views.MatchViewSet)
-router.register(r'competitions', views.CompetitionViewSet)
-router.register(r'competitions/(?P<competition_id>\d+)/matches',
+router.register(r'^users', views.UserViewSet)
+router.register(r'^persons', views.PersonViewSet)
+router.register(r'^players', views.PlayerViewSet)
+router.register(r'^matches', views.MatchViewSet)
+router.register(r'^competitions', views.CompetitionViewSet)
+router.register(r'^competitions/(?P<competition_id>\d+)/matches',
                 views.MatchPerCompetitionViewSet, base_name='competition')
 
 urlpatterns = router.urls
