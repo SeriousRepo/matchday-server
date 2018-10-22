a41aba46f6f304e821315d8469cfac157b9ef557
SeriousRepo <etekef.ro.man@gmail.com>
2018-10-22 21:32:25 +0200

    debug - off

--- matchday-server/settings.py
+++ matchday-server/settings.py
@@ -28,12 +28,12 @@ ALLOWED_HOSTS = []
 
 
 REST_FRAMEWORK = {
-    #'DEFAULT_RENDERER_CLASSES': (
-    #    'rest_framework.renderers.JSONRenderer',
-    #),
-    #'DEFAULT_PARSER_CLASSES': (
-    #    'rest_framework.parsers.JSONParser',
-    #),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+    ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
