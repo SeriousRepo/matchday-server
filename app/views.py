b6640a14fb637e4f4b8b2884f3d8a9b9b44f0532
SeriousRepo <etekef.ro.man@gmail.com>
2018-11-15 22:25:49 +0100

    changed queryset to distinct in teams per competition viewset

--- app/views.py
+++ app/views.py
@@ -90,7 +90,7 @@ class TeamsPerCompetition(viewsets.ReadOnlyModelViewSet):
 
     def get_queryset(self):
         competition_id = self.kwargs['competition_id']
-        return Team.objects.filter(team_in_match__match__competition=competition_id)
+        return Team.objects.filter(team_in_match__match__competition=competition_id).distinct()
 
 
 class TeamsInMatchesViewSet(viewsets.ModelViewSet):
